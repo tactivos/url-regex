fd0c566f9b17f3b20177bb68b49c30a50db5f6ff
Ezequiel Calderara <ecalderara@mural.co>
2021-06-03 16:43:03 -0300

    Fix eslint errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const ipRegex = require('ip-regex');
 const tlds = require('tlds');
 
-module.exports = function(options) {
+module.exports = function (options) {
 	options = Object.assign({strict: true}, options);
 
 	var protocol = `(?:(?:[a-z]+:)?//)${options.strict ? '' : '?'}`;
@@ -10,7 +10,9 @@ module.exports = function(options) {
 	var ip = ipRegex.v4().source;
 	var host = '(?:(?:[a-z\\u00a1-\\uffff0-9][-_]*)*[a-z\\u00a1-\\uffff0-9]+)';
 	var domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
-	var tld = `(?:\\.${options.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort(function (a, b) { return b.length - a.length;}).join('|')})`})\\.?`;
+	var tld = `(?:\\.${options.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort(function (a, b) {
+		return b.length - a.length;
+	}).join('|')})`})\\.?`;
 	var port = '(?::\\d{2,5})?';
 	var path = '(?:[/?#][^\\s"]*)?';
 	var regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
